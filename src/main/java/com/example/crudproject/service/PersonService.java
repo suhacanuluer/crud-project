6360ef00c1b732a3d5ae2da53cfb995ef674d326
suhacanuluer <sculuer@gmail.com>
2022-08-08 17:36:32 +0300

    @service annotation added

--- src/main/java/com/example/crudproject/service/PersonService.java
+++ src/main/java/com/example/crudproject/service/PersonService.java
@@ -1,9 +1,11 @@
 package com.example.crudproject.service;
 
 import com.example.crudproject.dto.PersonDto;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public interface PersonService {
 
     PersonDto save(PersonDto personDto);
