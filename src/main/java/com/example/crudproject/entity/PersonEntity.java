dcf3a27a4eb86a7b40f0fbbde66dbfbd2c56719e
suhacanuluer <sculuer@gmail.com>
2022-08-08 17:34:40 +0300

    lombok added

--- src/main/java/com/example/crudproject/entity/PersonEntity.java
+++ src/main/java/com/example/crudproject/entity/PersonEntity.java
@@ -1,26 +1,18 @@
 package com.example.crudproject.entity;
 
-import com.example.crudproject.dto.PersonDto;
 import lombok.*;
 
 import javax.persistence.*;
 
 @Entity
 @Table(name = "person")
+@AllArgsConstructor
 @NoArgsConstructor
 @Getter
 @Setter
 @EqualsAndHashCode(of = {"id"})
 @ToString
 public class PersonEntity {
-
-    public PersonEntity(String name, String surname, int age, String city, Boolean isStudent) {
-        this.name = name;
-        this.surname = surname;
-        this.age = age;
-        this.city = city;
-        this.isStudent = isStudent;
-    }
 
     @Id
     @SequenceGenerator(name = "seq_person", allocationSize = 1)
@@ -31,13 +23,4 @@ public class PersonEntity {
     private int age;
     private String city;
     private Boolean isStudent;
-    
-    public static PersonEntity of(PersonDto personDto) {
-        return new PersonEntity(personDto.getName(),
-                personDto.getSurname(),
-                personDto.getAge(),
-                personDto.getCity(),
-                personDto.getIsStudent());
-    }
-
 }
