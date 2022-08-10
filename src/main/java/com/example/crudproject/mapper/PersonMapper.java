6d4788413aac889cb8d8c1f8a3ec743004c9cdbf
suhacanuluer <sculuer@gmail.com>
2022-08-10 17:56:00 +0300

    personMapper edited about collectionMappingStrategy

--- src/main/java/com/example/crudproject/mapper/PersonMapper.java
+++ src/main/java/com/example/crudproject/mapper/PersonMapper.java
@@ -2,12 +2,21 @@ package com.example.crudproject.mapper;
 
 import com.example.crudproject.dto.PersonDto;
 import com.example.crudproject.entity.PersonEntity;
-import org.mapstruct.Mapper;
-import org.mapstruct.MappingTarget;
-import org.mapstruct.NullValuePropertyMappingStrategy;
+import com.example.crudproject.entity.TodoEntity;
+import org.mapstruct.*;
+import org.mapstruct.factory.Mappers;
 
-@Mapper(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE) // null gelirse eski veriyi tutuyor
+// null gelirse eski veriyi tutuyor
+@Mapper(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE,
+// Strategy for propagating the value of
+// collection-typed properties from source to target.
+//https://mapstruct.org/documentation/1.0/api/org/mapstruct/CollectionMappingStrategy.html
+        collectionMappingStrategy = CollectionMappingStrategy.ADDER_PREFERRED)
+
 public interface PersonMapper {
+
+    PersonMapper INSTANCE = Mappers.getMapper(PersonMapper.class);
+
     PersonEntity personDtoToPersonEntity(PersonDto personDto);
     PersonDto personEntityToPersonDto(PersonEntity personEntity);
     void updatePersonFromDto(PersonDto newPersonDto, @MappingTarget PersonEntity oldPersonEntity);
